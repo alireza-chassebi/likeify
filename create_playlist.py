083d92d1c157bde91e7239783f6b194aec1cbd57
alireza <alirezachassebi1@gmail.com>
2020-07-30 22:54:23 -0400

    completed create_playlist method of CreatePlaylist class

--- create_playlist.py
+++ create_playlist.py
@@ -1,7 +1,14 @@
+import json
+import requests
+
+
 class CreatePlaylist:
 
     def __init__(self):
-        pass
+        with open('secrets.json') as secrets_file:
+            secrets = json.load(secrets_file)
+            self.spotify_token = secrets["spotify_token"]
+            self.spotify_user_id = secrets["spotify_user_id"]
 
     def get_youtube_client(self):
         pass
@@ -10,7 +17,27 @@ class CreatePlaylist:
         pass
 
     def create_playlist(self):
-        pass
+
+        request_body = json.dumps({
+            'name': 'Liked YT Vids',
+            'description': 'All liked YT videos',
+            'public': True
+        })
+
+        url = "https://api.spotify.com/v1/users/{}/playlists".format(
+            self.spotify_user_id)
+
+        response = requests.post(
+            url,
+            data=request_body,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": "Bearer {}".format(self.spotify_token)
+            }
+        )
+
+        data = response.json()
+        return data["id"]  # playlist id
 
     def get_spotify_uri(self):
         pass

c914978b23b8c79d01c653cf102f58e43f4fdde5
alireza <alirezachassebi1@gmail.com>
2020-07-30 23:16:34 -0400

    completed get_spotify_uri method of CreatePlaylist

--- create_playlist.py
+++ create_playlist.py
@@ -16,6 +16,7 @@ class CreatePlaylist:
     def get_liked_videos(self):
         pass
 
+    # create spotify playlist
     def create_playlist(self):
 
         request_body = json.dumps({
@@ -39,8 +40,25 @@ class CreatePlaylist:
         data = response.json()
         return data["id"]  # playlist id
 
-    def get_spotify_uri(self):
-        pass
+    def get_spotify_uri(self, song_name, artist):
+
+        url = "https://api.spotify.com/v1/search?query=track%3A{}+artist%3A{}&type=track&offset=0&limit=20".format(
+            song_name, artist)
+
+        response = requests.get(
+            url,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": "Bearer {}".format(self.spotify_token)
+            }
+        )
+
+        data = response.json()
+        songs = data["tracks"]["items"]
+
+        # first song that matches
+        uri = songs[0]["uri"]
+        return uri
 
     def add_song_to_playlist(self):
         pass
